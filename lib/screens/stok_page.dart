7218c59aacc768577f85feca211358dce2ccafc0
tolunaykutlu <tolunaykt28@gmail.com>
2024-03-05 08:19:02 +0300

    last

--- lib/screens/stok_page.dart
+++ lib/screens/stok_page.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inventory_app/components/app_constants.dart';
